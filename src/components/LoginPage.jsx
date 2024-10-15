7297637d1e1db477a5dff3904e1e64167d994b5e
Jihyun <dns12134@gmail.com>
2024-10-16 02:48:09 +0900

    feat(login): 로그인 페이지 수정, react-router-dom 설치, 불필요한 코드 제거

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -1,24 +1,31 @@
 import { useUserStore } from "../store"
 import { signInWithPopup } from 'firebase/auth';
 import { auth, provider } from '../firebase';
+import { useNavigate } from 'react-router-dom';
 
 const LoginPage = () => {
   const setUser = useUserStore((state) => state.setUser);
+  const navigate = useNavigate();
 
   const handleLogin = async () => {
     try {
+      provider.setCustomParameters({
+        prompt: "select_account",
+      });
+      
       const result = await signInWithPopup(auth, provider);
       setUser(result.user); 
+      navigate('/documents');
     } catch (error) {
-      console.error('Login failed:', error);
+      console.error("로그인 실패", error);
     }
   };
 
   return (
     <div className="container">
-      <h1>Google Dogs</h1>
+      <h1>함께 문서 만들기</h1>
       <button onClick={handleLogin} className="loginButton">
-        Login by Google!
+        구글로 로그인하기
       </button>
     </div>
   );
--- src/store.js
+++ src/store.js
@@ -2,9 +2,6 @@ import { create } from 'zustand';
 
 export const useUserStore = create((set) => ({
   user: null,
-  isAuth: false,
-
-  setUser: (user) => set({ user: user, isAuth: true }),
-
-  logout: () => set({ user: null, isAuth: false }),
+  setUser: (user) => set({ user: user }),
+  clearUser: () => set({ user: null }), 
 }));
