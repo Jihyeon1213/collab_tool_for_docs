c247829d738c3b7fffca94c1bfe7e025abd24486
Jihyun <dns12134@gmail.com>
2024-10-18 05:10:40 +0900

    feat: react-router-dom을 이용한 라우팅 추가 및 CSS 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
-import LoginPage from './components/LoginPage'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import LoginPage from './components/LoginPage';
+import DocumentList from './components/DocumentList';
+import DocumentEditor from './components/DocumentEditor';
 
 function App() {
-
   return (
-    <>
-    <LoginPage />
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<LoginPage />} /> 
+        <Route path="/documents" element={<DocumentList />} /> 
+        <Route path="/edit/:documentKey" element={<DocumentEditor />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
+
